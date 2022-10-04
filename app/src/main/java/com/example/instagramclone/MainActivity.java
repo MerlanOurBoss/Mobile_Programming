7f496988eae02e9f4b3354f6130ec7f948ece92d
Merlan <44197642+MerlanOurBoss@users.noreply.github.com>
2022-10-04 19:02:00 +0600

    second commit
    add registration

--- app/src/main/java/com/example/instagramclone/MainActivity.java
+++ app/src/main/java/com/example/instagramclone/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.instagramclone;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +29,35 @@ public class MainActivity extends AppCompatActivity {
         login = findViewById(R.id.login);
 
         linearLayout.animate().alpha(0f).setDuration(1);
+
+        TranslateAnimation animation = new TranslateAnimation(0 ,  0 , 0 , -1000);
+        animation.setDuration(1000);
+        animation.setFillAfter(false);
+        animation.setAnimationListener(new MyAnimationListener());
+
+        iconImage.setAnimation(animation);
+
+        register.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, RegisterActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP)));
+
+        login.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, LoginActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_CLEAR_TOP)));
     }
 
+    public class MyAnimationListener implements Animation.AnimationListener {
+
+        @Override
+        public void onAnimationStart(Animation animation){
+
+        }
+        @Override
+        public void onAnimationEnd(Animation animation){
+
+            iconImage.clearAnimation();
+            iconImage.setVisibility(View.INVISIBLE);
+            linearLayout.animate().alpha(1f).setDuration(1000);
+        }
+        @Override
+        public void onAnimationRepeat(Animation animation){
+
+        }
+    }
 }
