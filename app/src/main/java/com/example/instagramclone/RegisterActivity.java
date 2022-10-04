c07e61987dab46bb20c9fe9cdb2ef0df2787e01b
Merlan <merlan01tel@gmail.com>
2022-10-05 01:17:24 +0600

    third commit
    connect to farebase

--- app/src/main/java/com/example/instagramclone/RegisterActivity.java
+++ app/src/main/java/com/example/instagramclone/RegisterActivity.java
@@ -14,7 +14,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -73,33 +75,36 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
-    private void registerUser(String username, String name, String email, String password) {
+    private void registerUser(final String username, final String name, final String email, String password) {
 
         pd.setMessage("Please Wait!");
         pd.show();
 
-        mAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(authResult -> {
-            HashMap<String, Object> map = new HashMap<>();
-            map.put("name", name);
-            map.put("email", email);
-            map.put("username", username);
-            map.put("id", mAuth.getCurrentUser().getUid());
+        mAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                HashMap<String, Object> map = new HashMap<>();
+                map.put("name", name);
+                map.put("email", email);
+                map.put("username", username);
+                map.put("id", mAuth.getCurrentUser().getUid());
 
-            mRootRef.child("Users").child(mAuth.getCurrentUser().getUid()).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task) {
-                    if (task.isSuccessful()) {
-                        pd.dismiss();
-                        Toast.makeText(RegisterActivity.this, "Update the profile " +
-                                "for better expereince", Toast.LENGTH_SHORT).show();
+                mRootRef.child("Users").child(mAuth.getCurrentUser().getUid()).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            pd.dismiss();
+                            Toast.makeText(RegisterActivity.this, "Update the profile " +
+                                    "for better experience", Toast.LENGTH_SHORT).show();
 
-                        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        startActivity(intent);
-                        finish();
+                            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+                            finish();
+                        }
                     }
-                }
-            });
+                });
+            }
         }).addOnFailureListener(e -> {
             pd.dismiss();
             Toast.makeText(RegisterActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
